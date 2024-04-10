7db1f212b73330255c2521f08b39e0ee992c651e
BlueDaisy061 <nttduong.c3tqcap.a1@gmail.com>
2024-04-10 23:13:29 +0700

    feat: update file upload function and set up storage

---
+++ backend/middleware/file-upload.js
@@ -0,0 +1,21 @@
+const multer = require("multer");
+const uuid = require("uuid/v4");
+
+const MIME_TYPE_MAP = {
+    "image/png": "png",
+    "image/jpeg": "jpeg",
+    "image/jpg": "jpg",
+};
+const fileUpload = multer({
+    limits: 500000,
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, "uploads/images");
+        },
+        filename: (req, file, cb) => {
+            const ext = MIME_TYPE_MAP[file.mimetype];
+        },
+    }),
+});
+
+module.exports = fileUpload;
